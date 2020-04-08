e85df811d31e0d027289b85c0d3f8d5a15a5fd00
Feng Ling <levincoolxyz@gmail.com>
2020-04-08 06:53:18 -0700

    Manual "merge" with upstream master branch
    picked up the new options on output and format
    changed ssh_cmd to function
    removed silly zenity dialog (I mean if I really need gui I should just install yad)
    make default landscape=false b/c I'm weird
    minor typos in comments

--- reStream.sh
+++ reStream.sh
@@ -1,29 +1,21 @@
 #!/bin/bash
 
 # default values for arguments
-ssh_host="10.11.99.1"      # remarkable connected trough USB
-landscape=true             # rotate 90 degrees to the right
+ssh_host="10.11.99.1"      # assume reMarkable is connected through USB
+landscape=false            # rotate window 90 deg cw for landscape view
+output_path=-              # display output through ffplay
+format=-                   # automatic output format
 
 # start gui for options if the system has yad or zenity
 if type yad >/dev/null && [ ! $(echo $DISPLAY) == "" ]; then
     output=$(yad --form --title "reMarkable streaming service" \
                  --field "ssh host" "10.11.99.1" \
-                 --field "landscape view":CHK TRUE \
+                 --field "landscape view":CHK FALSE \
                  --fixed --timeout=60 --separator " ")
-    # echo $output
+    if (( $? )); then exit 1; fi
     output=(${output})
     ssh_host=${output[0]}
     landscape=$(echo ${output[1]} | tr '[:upper:]' '[:lower:]')
-    # echo $ssh_host
-    # echo $landscape
-elif type zenity >/dev/null && [ ! $(echo $DISPLAY) == "" ]; then
-    ssh_host=$(zenity --title "reMarkable streaming service" \
-                      --entry --text "ssh host address?" --entry-text "10.11.99.1" \
-                       --timeout=60)
-    zenity --question --title "reMarkable streaming service" \
-           --text "view mode?" \
-           --ok-label "Landscape" --cancel-label "Portrait" --timeout=10
-    if (( ! $? )); then landscape=true; else landscape=false; fi
 else
 # loop through arguments and process them
 while [ $# -gt 0 ]; do
@@ -32,14 +24,35 @@ while [ $# -gt 0 ]; do
             landscape=false
             shift
             ;;
-        -d|--destination)
+        -l|--landscape)
+            landscape=true
+            shift
+            ;;
+        -s|--source)
             ssh_host="$2"
             shift
             shift
             ;;
-        *)
-            echo "Usage: $0 [-p] [-d <destination>]"
+        -o | --output)
+            output_path="$2"
+            shift
+            shift
+            ;;
+        -f | --format)
+            format="$2"
+            shift
+            shift
+            ;;
+        -h | --help | *)
+            echo "Usage: $0 [-p] [-s <source>] [-o <output>] [-f <format>]"
+            echo "Examples:"
+            echo "  $0                              # live view in landscape"
+            echo "  $0 -p                           # live view in portrait"
+            echo "  $0 -s 192.168.0.10              # connect to different IP"
+            echo "  $0 -o reMarkable.mp4            # record to a file"
+            echo "  $0 -o udp://dest:1234 -f mpegts # record to a stream"
             exit 1
+            ;;
     esac
 done
 fi
@@ -48,13 +61,15 @@ fi
 width=1408
 height=1872
 bytes_per_pixel=2
-loop_wait=true
-# loglevel=info
-loglevel=error
-ssh_cmd="ssh -o ConnectTimeout=1 "root@$ssh_host""
+loop_wait="sleep 0.05"
+# loglevel="info"
+loglevel="error"
+ssh_cmd() {
+    ssh -o ConnectTimeout=1 "root@$ssh_host" "$@"
+}
 
 # check if we are able to reach the remarkable
-if ! $ssh_cmd true; then
+if ! ssh_cmd true; then
     echo "reMarkable unreachable @ $ssh_host"
     exit 1
 fi
@@ -68,7 +83,7 @@ fi
 # }
 
 # check if lz4 is present on remarkable
-# if $ssh_cmd "[ -f \$HOME/lz4 ]"; then
+# if ssh_cmd "[ -f \$HOME/lz4 ]"; then
 #     compress="\$HOME/lz4"
 #     compress="/opt/bin/zstd --fast=200 -c"
 # fi
@@ -85,8 +100,17 @@ fi
 #     decompress="zstd -d"
 # fi
 
+# list of ffmpeg filters to apply
+video_filters=""
+
+# store extra ffmpeg arguments in $@
+set --
+
+# set each frame presentation time to the time it is received
+video_filters="$video_filters,setpts=(RTCTIME - RTCSTART) / (TB * 1000000)"
+
 # xor delta encoding & decoding with lz4 compression
-compress_only="\$HOME/lz4" # lz4 binary path on reMarkable
+compress_only="\$HOME/.bin/lz4" # lz4 binary path on reMarkable
 decompress_only="lz4 -d" # lz4 binary command on host
 xor="\$HOME/.bin/xorstream" # xorstream binary path on reMarkable
 tmpfile="/tmp/fb_old" # path where the reference frame buffer is stored
@@ -103,37 +127,53 @@ landscape_param="$($landscape && echo '-vf transpose=1')"
 head_fb0="dd if=/dev/fb0 count=1 bs=$window_bytes 2>/dev/null"
 
 # loop that keeps on reading and compressing, to be executed remotely
-read_loop="while $head_fb0; do sleep 0.05; done | $compress"
+read_loop="while $head_fb0; do $loop_wait; done | $compress"
 
-# # store initial frame buffer and transfer to host (turns out unnecessary)
-# $ssh_cmd "dd if=/dev/fb0 count=1 bs=$window_bytes of=$tmpfile"
-# $ssh_cmd "cat $tmpfile | $compress_only" | $decompress_only > $tmpfile
+# # store initial frame buffer and transfer to host (useless in 0.05 seconds)
+# ssh_cmd "dd if=/dev/fb0 count=1 bs=$window_bytes of=$tmpfile"
+# ssh_cmd "cat $tmpfile | $compress_only" | $decompress_only > $tmpfile
+
+set -- "$@" -vf "${video_filters#,}"
+
+if [ "$output_path" = - ]; then
+    output_cmd=ffplay
+else
+    output_cmd=ffmpeg
+
+    if [ "$format" != - ]; then
+        set -- "$@" -f "$format"
+    fi
+
+    set -- "$@" "$output_path"
+fi
 
 set -e # stop if an error occurs
 
 # # original solution by rien
 # # shellcheck disable=SC2086
-# # $ssh_cmd "$read_loop" \
-# #     | $decompress \
-# #     | ffplay -vcodec rawvideo \
-# #              -loglevel "$loglevel" \
-# #              -f rawvideo \
-# #              -pixel_format gray16le \
-# #              -video_size "$width,$height" \
-# #              $landscape_param \
-# #              -i -
+# ssh_cmd "$read_loop" \
+#     | $decompress \
+#     | "$output_cmd" \
+#         -vcodec rawvideo \
+#         -loglevel "$loglevel" \
+#         -f rawvideo \
+#         -pixel_format gray16le \
+#         -video_size "$width,$height" \
+#         -i - \
+#         "$@"
 
 # adding gui related flares (no need to ctrl-c once ffplay quits) + some ffmpeg flags
-$ssh_cmd "$read_loop" \
+ssh_cmd "$read_loop" \
     | $decompress_only | xorstream $tmpfile /dev/null d \
     | ( ffplay -fflags nobuffer -flags low_delay -framedrop \
-             -probesize 32 -sync ext -autoexit \
-             -window_title "reMarkable streaming service" \
-             -vcodec rawvideo \
-             -loglevel "$loglevel" \
-             -f rawvideo \
-             -pixel_format gray16le \
-             -video_size "$width,$height" \
-             $landscape_param \
-             -i - \
+               -probesize 32 -sync ext -autoexit \
+               -window_title "reMarkable streaming service" \
+               -vcodec rawvideo \
+               -loglevel "$loglevel" \
+               -f rawvideo \
+               -pixel_format gray16le \
+               -video_size "$width,$height" \
+               $landscape_param \
+               -i - \
+               "$@" \
     ; echo "streaming service stopped."; kill -15 $(ps -elf | grep "while dd if=/dev/fb0" | grep "root@$ssh_host" | awk '{print $4}') )
